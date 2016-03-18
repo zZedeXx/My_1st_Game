0d27b71955c6f4fd11d053a839afd2609acb72f3
denis <denhic123@gmail.com>
2016-03-18 17:43:50 +0600

    status_bar(1.0) + collide(0.1)

--- Classes/Hero.py
+++ Classes/Hero.py
@@ -1,6 +1,7 @@
 # inter_obj = {'✳':
 #
 # }
+import copy
 
 
 class Logs:
@@ -17,31 +18,43 @@ class Logs:
 
 
 class Hero:
-    def __init__(self, field, status_bar, logs):
+    def __init__(self, field, field2, status_bar, logs):
         self.x = None
         self.y = None
         self.image = 'H'
         self.Hit_Points = 100
-        self.damage = 100
+        self.Mana_Points = 100
+        self.Gold = 0
+        self.EXP = 0
+        self.level = 0
         self.key = 0
+        self.f = field2
         self.field = field
-        self.status_bar = status_bar
+        self.status_bar = copy.deepcopy(status_bar)
         self.template_status_bar = status_bar
         self.find_pos()
         self.logs = logs
         self.update()
 
     def draw_status(self):
+        # self.Hit_Points += 10
+        # self.logs.add_message(self.Hit_Points)
         self.status_bar[1][0] = self.template_status_bar[1][0].format(self.Hit_Points)
+        self.status_bar[2][0] = self.template_status_bar[2][0].format(self.Mana_Points)
+        self.status_bar[3][0] = self.template_status_bar[3][0].format(self.Gold)
+        self.status_bar[4][0] = self.template_status_bar[4][0].format(self.EXP)
+        self.status_bar[5][0] = self.template_status_bar[5][0].format(self.level)
         self.status_bar[6][0] = self.template_status_bar[6][0].format(self.key)
 
     def collide_objects(self):
-        if self.field[self.y][self.x] == "B":
+        if self.field[self.y][self.x] == "✳":
             self.Hit_Points -= 10
         elif self.field[self.y][self.x] == 'f':
             self.key += 1
         elif self.field[self.y][self.x] == '|':
             self.key -= 1
+        elif self.field[self.y][self.x] == '⬜':
+            self.field = self.f
 
     def find_pos(self):
         j = 0
@@ -74,10 +87,7 @@ class Hero:
             if self.look_down():
                 self.y += 1
 
-        if self.x >= len(self.field[0]):
-            self.x = 0
-        elif self.x < 0:
-            self.x = len(self.field[0]) - 1
+
 
     def look_left(self):
         if self.field[self.y][self.x - 1] == '#' or self.field[self.y][self.x - 1] == '|' and self.key == 0:
